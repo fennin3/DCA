6aa956620557d2f7f27247cbb74074ccd3591272
Francis Ennin <enninfrancis47@gmail.com>
2021-12-02 18:29:00 +0000

    make it better

--- DCA/settings.py
+++ DCA/settings.py
@@ -34,7 +34,7 @@ env = environ.Env(
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["datacareanalytics.herokuapp.com", "www.datacareanalytics.org","datacareanalytics.org" ]
 
