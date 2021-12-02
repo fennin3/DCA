47b02ad3e2f28bcf9ccd04fa327341f33cdff4eb
Francis Ennin <enninfrancis47@gmail.com>
2021-12-02 06:08:42 +0000

    first push

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about-us/", views.about, name="about"),
+    path("our-services/",views.services, name="services"),
+    path("contact-us/",views.contact, name="contact"),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home(request):
+    return render(request, "base/index.html")
+
+
+def about(request):
+    return render(request, 'base/about.html')
+
+
+def services(request):
+    return render(request, 'base/services.html')
+
+
+def contact(request):
+    return render(request, 'base/contact.html')
